91b2139b45fcead13bb4c1d52069eeb838bafae6
Opeyemi Oginni <opeyemi.oginni@heymoni.com>
2024-05-02 14:14:51 +0100

    Completed Scheduler for Oracle Contract

--- packages/xcm/src/moonbeam/moonbase-local.js
+++ packages/xcm/src/moonbeam/moonbase-local.js
@@ -4,13 +4,34 @@ import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { chains } from "@oak-network/config";
 import { askScheduleAction } from "../common/utils";
 import { scheduleTask } from "./common";
+import Web3 from "web3";
+import ethers from "ethers";
+
+const web3 = new Web3("http://localhost:8545");
 
 const INCREMENTER_CONTRACT_ADDRESS =
   "0xa72f549a1a12b9b49f30a7f3aeb1f4e96389c5d8";
 const INCREMENTER_CONTRACT_INPUT = "0xd09de08a";
 
-const ORACLE_CONTRACT_ADDRESS = "0x7f3";
-const ORACLE_CONTRACT_INPUT = "0x7f3";
+const ORACLE_CONTRACT_ADDRESS = "0xa72f549a1a12b9b49f30a7f3aeb1f4e96389c5d8";
+
+const functionName = "setTokenPrice"; // replace with your function name
+const parameterTypes = ["uint256"]; // replace with your parameter types
+const parameters = [ethers.parseEther(100)]; // replace with your parameters
+
+// Get the function signature
+const functionSignature = web3.eth.abi.encodeFunctionSignature(
+  `${functionName}(${parameterTypes.join(",")})`
+);
+
+// Encode the parameters
+const encodedParameters = web3.eth.abi.encodeParameters(
+  parameterTypes,
+  parameters
+);
+
+// Get the input for the contract
+const ORACLE_CONTRACT_INPUT = functionSignature + encodedParameters.slice(2); // remove the '0x' prefix from the encoded parameters
 
 // This is a Moonbeam test account private key. Please do not use it for any other purpose.
 // https://github.com/moonbeam-foundation/moonbeam/blob/2ea0db7c18d907ddeda1a5f4d3f68262e10560e7/README.md?plain=1#L65
@@ -48,17 +69,17 @@ const main = async () => {
     moonbeamKeyringPair,
   });
 
-  //   await scheduleTask({
-  //     oakConfig: turingLocal,
-  //     moonbeamConfig: moonbaseLocal,
-  //     scheduleActionType,
-  //     contract: {
-  //       address: ORACLE_CONTRACT_ADDRESS,
-  //       input: ORACLE_CONTRACT_INPUT,
-  //     },
-  //     keyringPair,
-  //     moonbeamKeyringPair,
-  //   });
+  await scheduleTask({
+    oakConfig: turingLocal,
+    moonbeamConfig: moonbaseLocal,
+    scheduleActionType,
+    contract: {
+      address: ORACLE_CONTRACT_ADDRESS,
+      input: ORACLE_CONTRACT_INPUT,
+    },
+    keyringPair,
+    moonbeamKeyringPair,
+  });
 };
 
 main()
@@ -67,3 +88,5 @@ main()
     console.log("Reached the end of main() ...");
     process.exit();
   });
+
+// INCREMENTER_CONTRACT_ADDRESS=<> ORACLE_CONTRACT_ADDRESS=<>  npm run moonbase-local
