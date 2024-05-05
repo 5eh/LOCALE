ec238e38d4422296964a64a279e7947b0983df82
5eh <38282105+5eh@users.noreply.github.com>
2024-05-05 17:19:27 -0500

    :zap: (./NextJS) Extended Designs

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal.tsx
@@ -13,15 +13,15 @@ export const AddressQRCodeModal = ({ address, modalId }: AddressQRCodeModalProps
       <div>
         <input type="checkbox" id={`${modalId}`} className="modal-toggle" />
         <label htmlFor={`${modalId}`} className="modal cursor-pointer">
-          <label className="modal-box relative">
+          <label className="modal-box backdrop-blur-xl relative border border-primary bg-primary/40">
             {/* dummy input to capture event onclick on modal box */}
             <input className="h-0 w-0 absolute top-0 left-0" />
             <label htmlFor={`${modalId}`} className="btn btn-ghost btn-sm btn-circle absolute right-3 top-3">
               ✕
             </label>
             <div className="space-y-3 py-6">
-              <div className="flex space-x-4 flex-col items-center gap-6">
-                <QRCodeSVG value={address} size={256} />
+              <div className="flex space-x-4 flex-col items-center  gap-6">
+                <QRCodeSVG value={address} size={256} className="border border-primary/20" />
                 <Address address={address} format="long" disableAddressLink />
               </div>
             </div>
