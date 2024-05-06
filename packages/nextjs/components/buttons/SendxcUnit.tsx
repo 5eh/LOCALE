61cb46515c19e0ed0fc429a923ea9c25f8bfa4a6
Opeyemi Oginni <107570612+OpeOginni@users.noreply.github.com>
2024-05-06 01:42:18 +0100

    Added Explanation Comment

--- packages/nextjs/components/buttons/SendxcUnit.tsx
+++ packages/nextjs/components/buttons/SendxcUnit.tsx
@@ -14,7 +14,10 @@ export default function SendxcUnitToken({ amount }: { amount: number }) {
   async function transferPaymentToRelay() {
     try {
     setIsPending(true)
+
     const currencyAddress = "0xFfFFfFff1FcaCBd218EDc0EbA20Fc2308C778080"; // xcUNIT address
+
+    // Sends token to address on the Relay Chain (This would be like Sending the Payment to Buyer's Polkadot)
     const destination = [
       1,
       ["0x01c4db7bcb733e117c0b34ac96354b10d47e84a006b9e7e66a229d174e8ff2a06300"],
