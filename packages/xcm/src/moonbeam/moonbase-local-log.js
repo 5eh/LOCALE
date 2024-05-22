4840429e564e44b95eb709bdbbc7e8c9cd9174ed
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-05-22 16:55:47 -0500

    :zap: (./MoonbaseAlpha) Temporarily removed to continue working on smart contract :zap:

--- packages/xcm/src/moonbeam/moonbase-local-log.js
+++
@@ -1,8 +0,0 @@
-import { askScheduleAction } from "../common/utils";
-import { scheduleTask } from "./common";
-import { chains } from "@oak-network/config";
-import Keyring from "@polkadot/keyring";
-import { hexToU8a } from "@polkadot/util";
-import { cryptoWaitReady } from "@polkadot/util-crypto";
-
-console.log(chains);
